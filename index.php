ba2691714cfb8b326d1fd3cbd74c943bd549b002
Sidi Sarr <sarrsidi177@gmail.com>
2026-03-30 14:50:46 +0000

    Initial commit - app gestion caisse simple

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['montant'])){
+    $montant = $_POST['montant'];
+    file_put_contents("database/db.txt", $montant . "\n" , FILE_APPEND);
+}
+$ventes = file("database/db.txt", FILE_IGNORE_NEW_LINES);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de caisse</title>
+</head>
+<body>
+    <h2>Ajouter une vente</h2>
+    <form>
+        <input type="number" name="montant" required="">
+        <button type="submit">Ajouter</button>
+    </form>
+    <h2>Liste des ventes</h2>
+    <ul>
+        <?php
+        $total = 0;
+        foreach ($ventes as $vente){
+            echo"<li>$vente MRU</li>";
+            $total += $vente; 
+        }
+        ?>
+    </ul>
+    <h3>Total : <?php echo $total; ?> MRU</h3>
+</body>
+</html>
